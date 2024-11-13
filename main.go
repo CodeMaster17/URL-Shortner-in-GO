0ff6038a7e4d8cd7d182a97ba57785484405354c
Harshit Yadav <2105891@kiit.ac.in>
2024-11-13 13:29:39 +0530

    feat: Generating hash

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"time"
+)
+
+type URL struct {
+	ID           string    `json:"id"`
+	OriginalURL  string    `json:"original_url"`
+	ShortURL     string    `json:"short_url"`
+	CreationDate time.Time `json:"creation_date"`
+}
+
+// keys of string type
+// values of URL type
+var UrlDB = make(map[string]URL)
+
+func generateShortURL(OriginalURL string) string {
+
+	// converts the given string to particular Hash value
+	hasher := md5.New()
+	hasher.Write([]byte(OriginalURL)) // converts given URL into bytes
+	data := hasher.Sum(nil)           // returns the hash value of the given URL
+	hash := hex.EncodeToString(data)  // converts the hash value into string
+	return hash[:8]                   // returns the first 8 characters of the hash value
+}
+
+
+func main(){
+	
+}
